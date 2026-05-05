0c9106f05e0edeb2b9505d07777e1dc5104fa7e9
Saita <saita@example.com>
2026-05-05 21:21:41 +0800

    Avoid Entity damage mixin startup crash

--- src/main/java/com/lastchance/downed/mixin/LivingEntityMixin.java
+++ src/main/java/com/lastchance/downed/mixin/LivingEntityMixin.java
@@ -1,8 +1,12 @@
 package com.lastchance.downed.mixin;
 
+import com.lastchance.downed.config.DownedPlayersConfig;
 import com.lastchance.downed.core.state.DownedStateManager;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerWorld;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -10,6 +14,30 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin {
+    @Inject(method = "applyDamage", at = @At("HEAD"), cancellable = true)
+    private void downedPlayers$interceptPlayerDamage(ServerWorld world, DamageSource source, float amount, CallbackInfo ci) {
+        Object self = (Object) this;
+        if (!(self instanceof ServerPlayerEntity player)) {
+            return;
+        }
+
+        if (DownedStateManager.isDeathBypassing(player)) {
+            return;
+        }
+
+        if (DownedStateManager.isDowned(player)) {
+            if (!DownedPlayersConfig.get().allow_downed_player_to_take_damage) {
+                ci.cancel();
+            }
+
+            return;
+        }
+
+        if (player.getHealth() - amount <= 0.0F && DownedStateManager.get().tryEnterDowned(player, source, amount)) {
+            ci.cancel();
+        }
+    }
+
     @Inject(method = "jump", at = @At("HEAD"), cancellable = true)
     private void downedPlayers$blockJump(CallbackInfo ci) {
         Object self = (Object) this;
